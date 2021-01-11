a5cdd3a04d94875cf0de5bb20f1c89cb41b1dafa
Paweł Walkowski <walkowski.pawel@wp.pl>
2021-01-11 22:00:04 +0100

    Add html, styles and script JS for navigation in media

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* Navigation in media */
+
+var icon = document.getElementById("icon");
+var icon1 = document.getElementById("a");
+var icon2 = document.getElementById("b");
+var icon3 = document.getElementById("c");
+var nav = document.getElementById('nav');
+var color = document.getElementById("color");
+
+icon.addEventListener('click', function() {
+  icon1.classList.toggle('a');
+  icon2.classList.toggle('c');
+  icon3.classList.toggle('b');  
+  nav.classList.toggle('show');
+  color.classList.toggle('slide');
+});

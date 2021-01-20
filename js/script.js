c09b3594f193db6753ac79b78ae51af33e065ee9
Paweł Walkowski <walkowski.pawel@wp.pl>
2021-01-20 23:59:26 +0100

    Change html and styles for header, logo, go top icon, intro. Change html, styles and script JS for menu

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-/* Navigation in media */
+/* Menu in media */
 
-var icon = document.getElementById("icon");
-var icon1 = document.getElementById("a");
-var icon2 = document.getElementById("b");
-var icon3 = document.getElementById("c");
-var nav = document.getElementById('nav');
-var color = document.getElementById("color");
+const icon = document.getElementById("icon");
+const icon1 = document.getElementById("a");
+const icon2 = document.getElementById("b");
+const icon3 = document.getElementById("c");
+const menu = document.getElementById('menu');
+const color = document.getElementById("color");
 
 icon.addEventListener('click', function() {
   icon1.classList.toggle('a');
   icon2.classList.toggle('c');
   icon3.classList.toggle('b');  
-  nav.classList.toggle('show');
+  menu.classList.toggle('show');
   color.classList.toggle('slide');
 });

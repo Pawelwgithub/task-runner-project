45dedf148d4b3e11f0b5a21b93ed06d9539552f4
Paweł Walkowski <walkowski.pawel@wp.pl>
2021-02-05 23:11:31 +0100

    Add html and script JS jQuery for scroll to top. Change html and styles for scroll to top

--- js/script.js
+++ js/script.js
@@ -14,3 +14,22 @@ icon.addEventListener('click', function() {
   menu.classList.toggle('show');
   color.classList.toggle('slide');
 });
+
+/* Scroll to top */
+
+$(function() {
+         
+  var icon = $('.scroll-to-top-icon i');
+
+  $(window).scroll(function() {
+      if ($(window).scrollTop() > 90) {
+          icon.addClass('show');
+      } else {
+          icon.removeClass('show');
+      }
+  });
+
+  icon.click(function() {
+      $('html, body').animate({scrollTop: 0}, 400, 'linear');
+  });
+});

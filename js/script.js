d000dddc48be7f1a57018faf9e1a40884903b6c5
Paweł Walkowski <walkowski.pawel@wp.pl>
2022-01-16 16:36:17 +0100

    Add ESLint and EditorConfig. Fix scripts JS and jQuery for errors

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 /* Menu in media */
 
-var icon = document.getElementById("icon");
-var icon1 = document.getElementById("a");
-var icon2 = document.getElementById("b");
-var icon3 = document.getElementById("c");
+var icon = document.getElementById('icon');
+var icon1 = document.getElementById('a');
+var icon2 = document.getElementById('b');
+var icon3 = document.getElementById('c');
 var menu = document.getElementById('menu');
-var color = document.getElementById("color");
+var color = document.getElementById('color');
 
 icon.addEventListener('click', function() {
   icon1.classList.toggle('a');
@@ -22,14 +22,14 @@ $(function() {
   var icon = $('.scroll-to-top-icon i');
 
   $(window).scroll(function() {
-      if ($(window).scrollTop() > 90) {
-          icon.addClass('show');
-      } else {
-          icon.removeClass('show');
-      }
+    if ($(window).scrollTop() > 90) {
+      icon.addClass('show');
+    } else {
+      icon.removeClass('show');
+    }
   });
 
   icon.click(function() {
-      $('html, body').animate({scrollTop: 0}, 400, 'linear');
+    $('html, body').animate({scrollTop: 0}, 400, 'linear');
   });
 });
